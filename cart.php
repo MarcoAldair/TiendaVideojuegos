f2f2c9493adb2d29c16d39afd873bbef0f976cf8
Saul4342 <76011097+Saul4342@users.noreply.github.com>
2021-06-25 13:51:29 -0500

    Add files via upload

--- cart.php
+++ cart.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Carrito de compras</title>
+	<link rel="stylesheet" type="text/css" href="style/style_cart.css">
 </head>
 <?php
 	include('cartClass.php');
@@ -25,13 +26,28 @@
 	if(isset($_GET['view']) && isset($_SESSION['elemtosCarrito'])){
 ?>
 <body>
-	<table border="1">
+	<!--MENU-->
+
+	<div class="cabecera">
+		<div class="nav">
+			<a href="index.php" class="logo">Indie World</a>
+			<ul class="nav-menu">
+				<li><a href="index.php" class="nav-menu-item">Inicio</a></li>
+				<li><a href="viewCatalog.php" class="nav-menu-item">Catalogo</a></li>
+				<li><a href="cart.php?view" class="nav-menu-item">Carrito</a></li>
+			</ul>
+			
+		</div>
+	</div>
+	<!--Carrito-->
+	<h2>Carrito de compra</h2>
+	<table>
 		<thead>
-			<th>Nombre</th>
+			<th>Juego</th>
 			<th>Precio</th>
 			<th>Cantidad</th>
 			<th>total</th>
-			<th></th>
+			<th>Eliminar</th>
 		</thead>
 		<tbody>
 			<?php foreach ($_SESSION['elemtosCarrito'] as $key => $cart) {?>			
@@ -40,19 +56,25 @@
 				<td><?php echo $cart->getGamePrice() ;?></td>
 				<td><?php echo $cart->getGameQuantity() ;?></td>
 				<td><?php echo $cart->getGameTotal() ;?></td>
-				<td><a href="?delete=<?php echo $cart->getGameId(); ?>">Eliminar</a></td>
+				<td><a href="?delete=<?php echo $cart->getGameId(); ?>"><img src="imagen/delete.png" height = "26"></a></td>
 			</tr>
 			<?php }?>
 		</tbody>
 	</table>
+	<form action="index.php">
+		<input type="submit" name="" value="Volver" class="button">
+	</form>
 </body>
 </html>
 <?php
 	}else{
 ?>
 <body>
-	<h1>Tu carrito esta vacio</h1>
-	<a href="index.php">volver</a>
+	<br>
+	<h2 id="carritoVacio">Tu carrito esta vacio</h2>
+	<form action="index.php">
+		<input type="submit" name="" value="Volver" class="button">
+	</form>
 </body>
 </html>
 <?php		
