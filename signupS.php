acbf43c719e403d9ea4a53f08d41cdb32d764fab
SaulAlamilla <76011097+Saul4342@users.noreply.github.com>
2021-06-29 03:24:05 -0500

    Add files via upload

--- signupS.php
+++ signupS.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Elegir opcion</title>
+    <link rel="stylesheet" href="style/style_login.css">
 </head>
 <body>
-    <h1>Seleccione como que tipo de usuario desea registrarse</h1>
+    <h1 class="type-user">Seleccione como que tipo de usuario desea registrarse</h1>
     <nav>
-        <a href="signupCus.php">Cliente</a>
-        <a href="signupAdmin.php">Encargado verificacion</a>
-        <a href="signupDev.php">Desarrollador</a>
+        <a href="signupCus.php" class="tipo">Cliente</a>
+        <a href="signupAdmin.php" class="tipo">Encargado verificacion</a>
+        <a href="signupDev.php" class="tipo">Desarrollador</a>
     </nav>
 </body>
 </html>
