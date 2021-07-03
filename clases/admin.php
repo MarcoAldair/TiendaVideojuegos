b1d820cb985475969b6efd8a7b4ad949c8b646d7
MarcoAldair <caceresmajc@gmail.com>
2021-07-03 15:03:18 -0500

    Clase Encargado

---
+++ clases/admin.php
@@ -0,0 +1,67 @@
+<?php
+class developer{
+    private $idEncargado;
+    private $nombre;
+    private $apellido1 ;
+    private $apellido2;
+    private $telefono;
+    private $idUser;
+    private $userName;
+    
+    public function getIdEncargado(){
+		return $this->idEncargado;
+	}
+
+	public function setIdEncargado($idDev){
+		$this->idDev = $idEncargado;
+	}
+
+	public function getNombre(){
+		return $this->nombre;
+	}
+
+	public function setNombre($nombre){
+		$this->nombre = $nombre;
+	}
+
+	public function getApellido1(){
+		return $this->apellido1;
+	}
+
+	public function setApellido1($apellido1){
+		$this->apellido1 = $apellido1;
+	}
+
+	public function getApellido2(){
+		return $this->apellido2;
+	}
+
+	public function setApellido2($apellido2){
+		$this->apellido2 = $apellido2;
+	}
+
+	public function getTelefono(){
+		return $this->telefono;
+	}
+
+	public function setTelefono($telefono){
+		$this->telefono = $telefono;
+	}
+
+	public function getIdUser(){
+		return $this->idUser;
+	}
+
+	public function setIdUser($idUser){
+		$this->idUser = $idUser;
+	}
+
+	public function getUserName(){
+		return $this->userName;
+	}
+
+	public function setUserName($userName){
+		$this->userName = $userName;
+	}
+}
+?>
